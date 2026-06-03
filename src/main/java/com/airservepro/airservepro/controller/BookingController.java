b5f39837ebfbfd96aab821d9def8317851b3ef0f
Gurucharan M <maripalagurucharan@gmail.com>
2026-06-03 18:17:27 +0530

    added Request Body decorator to POST sub controller of Booking

--- src/main/java/com/airservepro/airservepro/controller/BookingController.java
+++ src/main/java/com/airservepro/airservepro/controller/BookingController.java
@@ -4,6 +4,7 @@ package com.airservepro.airservepro.controller;
 import com.airservepro.airservepro.dto.BookingRequestDTO;
 import com.airservepro.airservepro.service.BookingService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class BookingController {
     }
 
     @PostMapping("/create")
-    public String createBooking(BookingRequestDTO dto)
+    public String createBooking(@RequestBody BookingRequestDTO dto)
     {
         return bookingService.createBooking(dto);
     }
